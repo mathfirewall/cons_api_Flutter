1946572c6b96314208e736e3044cc63cc4375893
mathfirewall <thiagodevmobile@gmail.com>
2022-05-15 16:55:20 -0400

    evolução 1.0

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -12,7 +12,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   //função que chama o json via GET
   Future<List> pegaUsers() async {
     var url = Uri.parse('https://jsonplaceholder.typicode.com/todos');
@@ -31,40 +30,48 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Usuários'),
+          title: Text(
+            'Usuários',
+            style: TextStyle(fontSize: 40),
+          ),
         ),
         // aqui cria uma list e chama a função acima
-        body: FutureBuilder<List>(
-          future: pegaUsers(),
-          builder: (context, snapshot) {
-            // verifica se deu algum erro 
-            if (snapshot.hasError) {
-              return Center(
-                child: Text('Erro ao Carregar Usuários'),
-              );
-            }
-            //aqui como foi tudo OK ele passa a chamar e mostrar a consulta
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text("UserId: " +
-                          snapshot.data![index]['userId'].toString() +
-                          "\nId: " +
-                          snapshot.data![index]['id'].toString() +
-                          "\nTitulo: " +
-                          snapshot.data![index]['title'].toString() +
-                          "\nCompleto: " +
-                          snapshot.data![index]['completed'].toString()),
-                    );
-                  });
-            }
-            // essa função se caso aja algum problema no carregamento ele mostra o carregamento em Tela
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-        ));
+        body: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: FutureBuilder<List>(
+              future: pegaUsers(),
+              builder: (context, snapshot) {
+                // verifica se deu algum erro
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Erro ao Carregar Usuários'),
+                  );
+                }
+                //aqui como foi tudo OK ele passa a chamar e mostrar a consulta
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        final dados = snapshot.data![index];
+                        return ListTile(
+                          
+                          title: Text(
+                              "Titulo: " +
+                                  dados['title'].toString(),
+                              style: TextStyle(fontSize: 20, color: Colors.blueAccent)),
+                          subtitle: Text(
+                              "Completed: " +
+                                  dados['completed'].toString(),
+                              style: TextStyle(fontSize: 16, color: Colors.black87)),
+                        );
+                      });
+                }
+                // essa função se caso aja algum problema no carregamento ele mostra o carregamento em Tela
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+            )));
   }
 }
